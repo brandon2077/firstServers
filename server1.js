6aed1d8ad7b55a8359b9445925fc71a924e8260b
brandonao <brandonowensgd@gmail.com>
2018-05-14 19:52:12 -0700

    add index.html

---
+++ server1.js
@@ -0,0 +1,22 @@
+//Define the port for incoming requests
+const PORT = 7500;
+
+// Creating a generic function to handle requests and responses
+function handleRequest(request, response) {
+
+// Send the below string to the client when the user visits to the PORT URL
+	response.end("You're basically fucking stupid!");
+};
+
+// Using the Node HTTP package to create our server.
+// Pass the handleRequest function to empower it with functionality
+const server = http.createServer(handleRequest);
+
+// Start our server so that it can begin listening to our client requests
+server.listen(PORT, function() {
+
+// Log (server-side) when our client has started
+	console.log("Server listening on: https://localhost:" + PORT);
+
+});
+
